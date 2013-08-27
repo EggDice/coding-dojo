cb28df8509f61a9fc80494602865758c72a93af3
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2013-08-27 13:09:24 -0400

    roman_number kata js pomodoro 2

--- roman_numerals/js/kata.js
+++ roman_numerals/js/kata.js
@@ -1,14 +1,32 @@
+var RULES = [
+  {symbol: 'X', value: 10, repeat: true},
+  {symbol: 'IX', value: 9},
+  {symbol: 'V', value: 5},
+  {symbol: 'IV', value: 4},
+  {symbol: 'I', value: 1, repeat: true}
+];
+
 function normal2roman(normal) {
   if (!normal) {
     return '';
   }
   var output = '';
-  if (normal >= 5) {
-    output += 'V';
-    normal -= 5;
-  }
-  for (var i = 0; i < normal; ++i) {
-    output += 'I';
-  }
+  RULES.sort(function(a, b) {
+    return b.value - a.value;
+  }).forEach(function(rule) {
+    if (rule.repeat) {
+      while (normal >= rule.value) {
+        output += rule.symbol;
+        normal -= rule.value;
+      }
+    } else {
+      if (normal >= rule.value) {
+        output += rule.symbol;
+        normal -= rule.value;
+      }
+    }
+  });
   return output;
 }
+
+
--- roman_numerals/js/test.js
+++ roman_numerals/js/test.js
@@ -13,15 +13,41 @@ describe('normal2roman converter', function() {
     expect(normal2roman()).toBe('');
   });
 
-  it('should return I', function() {
-    expect(normal2roman(1)).toBe('I');
-  });
+  describe('one letters', function() {
+    it('should return I', function() {
+      expect(normal2roman(1)).toBe('I');
+    });
 
-  it('should return V', function() {
-    expect(normal2roman(5)).toBe('V');
+    it('should return V', function() {
+      expect(normal2roman(5)).toBe('V');
+    });
+
+    it('should return X', function() {
+      expect(normal2roman(10)).toBe('X');
+    });
   });
 
   it('should return III', function() {
     expect(normal2roman(3)).toBe('III');
+  });
+
+  it('should return IV', function() {
+    expect(normal2roman(4)).toBe('IV');
+  });
+
+  it('should return VII', function() {
+    expect(normal2roman(7)).toBe('VII');
+  });
+
+  it('should return IX', function() {
+    expect(normal2roman(9)).toBe('IX');
+  });
+
+  it('should return XVI', function() {
+    expect(normal2roman(16)).toBe('XVI');
+  });
+
+  it('should return XX', function() {
+    expect(normal2roman(20)).toBe('XX');
   });
 });
