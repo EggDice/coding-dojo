5a786f4d057b0f2817bd87b7554eb7166ed06b8a
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2013-09-04 12:29:30 -0400

    roman_number kata js pomodoro 5

--- roman_numerals/js/kata.js
+++ roman_numerals/js/kata.js
@@ -2,66 +2,105 @@ var BASE_RULES = {
   '1': 'I',
   '5': 'V',
   '10': 'X',
-  '50': 'L'
+  '50': 'L',
+  '100': 'C',
+  '500': 'D',
+  '1000': 'M'
 };
 
-function normal2roman(normal) {
-  if (!normal) { return ''; }
-  var output = '';
-  createRules(BASE_RULES).forEach(function(rule) {
-    while (normal >= rule.value) {
-      output += rule.symbol;
-      normal -= rule.value;
-    }
-  });
-  return output;
-}
+var allRules = [];
 
-function createRules(baseRules) {
-  var base_values = Object.keys(BASE_RULES).map(toInt);
-  var rules = [];
-  for (var i = 0, l = base_values.length; i < l; ++i) {
-    var value = base_values[i];
-    var nextValue = base_values[i + 1];
-    var prevValue = base_values[i - 1];
-    rules.push(createSimpleRule(value));
-    if (i == l) { break; }
-    if (is10exponent(value)) {
-      rules.push(createComplexRule(nextValue, value));
-    } else {
-      rules.push(createComplexRule(nextValue, prevValue));
+function normal2roman(normal) {
+  var roman = '';
+  return convert();
+
+  function convert() {
+    checkArgument();
+    if (!normal) {
+      return '';
+    }
+    generateRules();
+    allRules.forEach(reduceRule);
+    return roman;
+  }
+
+  function checkArgument() {
+    if (normal > 3000) {
+      throw new Error('Argument bigger than 3000');
+    }
+    if (normal < 1) {
+      throw new Error('Argument smaller than 1');
     }
   }
-  return rules.sort(sortByValue);
+
+  function reduceRule(rule) {
+    while (normal >= rule.value) {
+      roman += rule.symbol;
+      normal -= rule.value;
+    }
+  }
+
+  function generateRules() {
+    if (!allRules.length) {
+      var baseValues = Object.keys(BASE_RULES).map(toInt);
+      createRulesByBaseValues(baseValues);
+      allRules.sort(sortByValue);
+    }
+  }
+
+  function createRulesByBaseValues(baseValues) {
+    for (var i = 0, l = baseValues.length; i < l; ++i) {
+      var value = baseValues[i];
+      var nextValue = baseValues[i + 1];
+      var prevValue = baseValues[i - 1];
+      addSimpleRule(value);
+      if (i != l) {
+        addComplexRule(value, nextValue, prevValue);
+      }
+    }
+  }
+
+  function addSimpleRule(value) {
+    allRules.push(createSimpleRule(value));
+  }
+
+  function addComplexRule(value, nextValue, prevValue) {
+    if (is10exponent(value)) {
+      allRules.push(createComplexRule(nextValue, value));
+    } else {
+      allRules.push(createComplexRule(nextValue, prevValue));
+    }
+  }
+
+  function createComplexRule(biggerNumber, substractedNumber) {
+    return {
+      'value': biggerNumber - substractedNumber,
+      'symbol': BASE_RULES[substractedNumber] + BASE_RULES[biggerNumber]
+    };
+  }
+
+  function createSimpleRule(number) {
+    return {
+      'value': number,
+      'symbol': BASE_RULES[number]
+    };
+  }
+
+  function is10exponent(number) {
+    var log = log10(number);
+    return Math.floor(log) === log;
+  }
+
+  function log10(val) {
+    return Math.log(val) / Math.LN10;
+  }
+
+  function sortByValue(a, b) {
+    return b.value - a.value;
+  }
+
+  function toInt(string) {
+    return parseInt(string, 10);
+  }
 }
 
-function createComplexRule(biggerNumber, substractedNumber) {
-  return {
-    'value': biggerNumber - substractedNumber,
-    'symbol': BASE_RULES[substractedNumber] + BASE_RULES[biggerNumber]
-  };
-}
-
-function createSimpleRule(number) {
-  return {
-    'value': number,
-    'symbol': BASE_RULES[number]
-  };
-}
-
-function is10exponent(number) {
-  var log = log10(number);
-  return Math.floor(log) === log;
-}
-
-function log10(val) {
-  return Math.log(val) / Math.LN10;
-}
-
-function sortByValue(a, b) {
-  return b.value - a.value;
-}
-
-function toInt(string) {
-  return parseInt(string, 10);
-}
--- roman_numerals/js/test.js
+++ roman_numerals/js/test.js
@@ -13,6 +13,18 @@ describe('normal2roman converter', function() {
     expect(normal2roman()).toBe('');
   });
 
+  it('should throw error if the number is bigger than 3000', function() {
+    expect(function() {
+      normal2roman(3001);
+    }).toThrow();
+  });
+
+  it('should throw error if the number is smaller than 1', function() {
+    expect(function() {
+      normal2roman(0);
+    }).toThrow();
+  });
+
   describe('one letters', function() {
     it('should return I', function() {
       expect(normal2roman(1)).toBe('I');
@@ -29,33 +41,60 @@ describe('normal2roman converter', function() {
     it('should return L', function() {
       expect(normal2roman(50)).toBe('L');
     });
+
+    it('should return C', function() {
+      expect(normal2roman(100)).toBe('C');
+    });
+
+    it('should return D', function() {
+      expect(normal2roman(500)).toBe('D');
+    });
+
+    it('should return M', function() {
+      expect(normal2roman(1000)).toBe('M');
+    });
   });
 
-  it('should return III', function() {
-    expect(normal2roman(3)).toBe('III');
+  describe('combinations', function() {
+    it('should return III', function() {
+      expect(normal2roman(3)).toBe('III');
+    });
+
+    it('should return IV', function() {
+      expect(normal2roman(4)).toBe('IV');
+    });
+
+    it('should return VII', function() {
+      expect(normal2roman(7)).toBe('VII');
+    });
+
+    it('should return IX', function() {
+      expect(normal2roman(9)).toBe('IX');
+    });
+
+    it('should return XVI', function() {
+      expect(normal2roman(16)).toBe('XVI');
+    });
+
+    it('should return XX', function() {
+      expect(normal2roman(20)).toBe('XX');
+    });
+
+    it('should return XL', function() {
+      expect(normal2roman(40)).toBe('XL');
+    });
+
+    it('should return XL', function() {
+      expect(normal2roman(473)).toBe('CDLXXIII');
+    });
+
+    it('should return MCMXCIX', function() {
+      expect(normal2roman(1999)).toBe('MCMXCIX');
+    });
   });
 
-  it('should return IV', function() {
-    expect(normal2roman(4)).toBe('IV');
-  });
-
-  it('should return VII', function() {
-    expect(normal2roman(7)).toBe('VII');
-  });
-
-  it('should return IX', function() {
-    expect(normal2roman(9)).toBe('IX');
-  });
-
-  it('should return XVI', function() {
-    expect(normal2roman(16)).toBe('XVI');
-  });
-
-  it('should return XX', function() {
-    expect(normal2roman(20)).toBe('XX');
-  });
-
-  it('should return XL', function() {
-    expect(normal2roman(40)).toBe('XL');
+  it('should run independent', function() {
+    expect(normal2roman(1999)).toBe('MCMXCIX');
+    expect(normal2roman(1)).toBe('I');
   });
 });
